923bd6baa8d376f0e8310165ad10d55c9a8fc7df
ar2-url <57313716+ar2-url@users.noreply.github.com>
2021-01-22 12:50:57 +0000

    v.3.0.1

---
+++ add.js
@@ -0,0 +1,19 @@
+$('#countrySelect').html('');
+
+ 
+
+$.each(result.data, function(index) {
+
+ 
+
+    $('#countrySelect').append($("<option>", {
+
+        value: result.data[index].code,
+
+        text: result.data[index].name
+
+    })); 
+
+ 
+
+}); 
