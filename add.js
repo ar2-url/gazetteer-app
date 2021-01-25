b071ebe848bbebf02b00ddfdf1d82a846170f501
ar2-url <57313716+ar2-url@users.noreply.github.com>
2021-01-25 13:03:41 +0000

    v3.0.2

--- add.js
+++ add.js
@@ -4,6 +4,22 @@ $('#countrySelect').html('');
 
 $.each(result.data, function(index) {
 
+
+    // I try to add connection to php file via ajax to your part of code here 
+    // $.ajax({
+    //     url: "php/ctr.php",
+    //     type: 'POST',
+    //     dataType: 'json',
+    //     data: {
+    //         country: $('#countrySelect').val()
+    //     },
+    //     success: function(result) {
+
+    //         console.log(result);
+
+    //         if (result.status.name == "ok") {
+
+        //up to here
  
 
     $('#countrySelect').append($("<option>", {
@@ -12,8 +28,11 @@ $.each(result.data, function(index) {
 
         text: result.data[index].name
 
+
     })); 
 
  
 
 }); 
+
+
--- ctr.php
+++ ctr.php
@@ -67,29 +67,3 @@
  
 
 ?>
-
- 
-
- 
-
- 
-
-$('#countrySelect').html('');
-
- 
-
-$.each(result.data, function(index) {
-
- 
-
-    $('#countrySelect').append($("<option>", {
-
-        value: result.data[index].code,
-
-        text: result.data[index].name
-
-    })); 
-
- 
-
-}); 
