ed10db2fc1b597fb438804499cddb6dedbd5d7a1
ar2-url <57313716+ar2-url@users.noreply.github.com>
2021-02-02 22:17:33 +0000

    v3.0.6

--- js/script.js
+++ js/script.js
@@ -1,4 +1,88 @@
-	$('#countrySelect').html('');
+// map
+
+// Initialize the map 
+const map = L.map('map');
+  
+// Get the tile layer from OpenStreetMaps 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { 
+
+// Specify the maximum zoom of the map 
+maxZoom: 19, 
+
+// Set the attribution for OpenStreetMaps 
+attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' 
+}).addTo(map); 
+
+// Set the view of the map 
+// with the latitude, longitude and the zoom value 
+//map.setView([48.8584, 2.2945], 16); 
+  
+
+
+// Set the map view to the user's location 
+// Uncomment below to set map according to user location 
+map.locate({setView: true, maxZoom: 13}); 
+
+
+
+function onLocationFound(e) {
+  var radius = e.accuracy / 2;
+  L.marker(e.latlng).addTo(map)
+	.bindPopup("You are within " + radius + " meters from this point").openPopup();
+  L.circle(e.latlng, radius).addTo(map);
+
+}
+
+map.on('locationfound', onLocationFound);
+map.locate({setView: true, watch: true, maxZoom: 13});
+
+
+//ajax call
+
+
+$(document).ready(function(){
+
+ 
+
+  $.ajax({
+
+      type: 'POST',
+
+      url: 'php/ctr.php',
+
+      dataType: 'json',
+
+      success: function(result) {
+
+
+
+          $('#countrySelect').html('');
+
+
+
+          $.each(result.data, function(index) {
+
+          
+
+              $('#countrySelect').append($("<option>", {
+
+                  value: result.data[index].code,
+
+                  text: result.data[index].name
+
+              })); 
+
+          
+
+          }); 
+
+      }
+
+  });
+
+
+
+$('#countrySelect').html('');
 
  
 
@@ -35,7 +119,7 @@ function change_countrySelect(sel) {
         document.getElementById("show").innerHTML = txt;
         }
       };
-    xmlhttp.open("POST", "../php/ctr.php", true);
+    xmlhttp.open("POST", "php/ctr.php", true);
     xmlhttp.setRequestHeader("Content-type", "application/json");
     xmlhttp.send("x=" + dbParam);
   }
@@ -102,40 +186,5 @@ function clickOutside(e){
     
 };
 
-// map
-
-// Initialize the map 
-const entryMap = L.map('entryMap');
-  
-// Get the tile layer from OpenStreetMaps 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { 
-
-// Specify the maximum zoom of the map 
-maxZoom: 19, 
-
-// Set the attribution for OpenStreetMaps 
-attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' 
-}).addTo(entryMap); 
-
-// Set the view of the map 
-// with the latitude, longitude and the zoom value 
-//map.setView([48.8584, 2.2945], 16); 
-  
 
 
-// Set the map view to the user's location 
-// Uncomment below to set map according to user location 
-entryMap.locate({setView: true, maxZoom: 13}); 
-
-
-
-function onLocationFound(e) {
-  var radius = e.accuracy / 2;
-  L.marker(e.latlng).addTo(entryMap)
-	.bindPopup("You are within " + radius + " meters from this point").openPopup();
-  L.circle(e.latlng, radius).addTo(entryMap);
-
-}
-
-entryMap.on('locationfound', onLocationFound);
-entryMap.locate({setView: true, watch: true, maxZoom: 13});
