a2f7b65c2ba517663fcb0cad31a8699ff28bf971
ar2-url <57313716+ar2-url@users.noreply.github.com>
2021-02-08 09:06:17 +0000

    v3.0.6

--- js/script.js
+++ js/script.js
@@ -1,99 +1,155 @@
-	$('#btnRun').click(function() {
+	// map
 
-		$.ajax({
-			url: "php/getCountryInfo.php",
-			type: 'POST',
-			dataType: 'json',
-			data: {
-				country: $('#selCountry').val(),
-				lang: $('#selLanguage').val()
-			},
-			success: function(result) {
+// Initialize the map 
+const map = L.map('map');
+  
+// Get the tile layer from OpenStreetMaps 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { 
 
-				console.log(result);
+// Specify the maximum zoom of the map 
+maxZoom: 19, 
 
-				if (result.status.name == "ok") {
+// Set the attribution for OpenStreetMaps 
+attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' 
+}).addTo(map); 
 
-					$('#txtContinentName').html(result['data'][0]['continentName']);
-					$('#txtCountryName').html(result['data'][0]['countryName']);
-					$('#txtCapital').html(result['data'][0]['capital']);
-					$('#txtLanguages').html(result['data'][0]['languages']);
-					$('#txtAreaInSqKm').html(result['data'][0]['areaInSqKm']);
-					$('#txtPopulation').html(result['data'][0]['population']);
-					$('#txtCurrencyCode').html(result['data'][0]['currencyCode']);
+// Set the view of the map on point
+// with the latitude, longitude and the zoom value 
+//map.setView([48.8584, 2.2945], 16);  
 
-				}
-			
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				// your error code
-			}
-		}); 
-	
+// Set the map view to the user's location  
+map.locate({setView: true, maxZoom: 13}); 
 
-	});
 
-	$('#btnRun2').click(function() {
 
-		$.ajax({
-			url: "php/getWikiInfo.php",
-			type: 'POST',
-			dataType: 'json',
-			data: {
-				maxRows: $('#selRows').val(),
-				q: $('#selQuery').val()
-			},
-			success: function(result) {
+function onLocationFound(e) {
+  var radius = e.accuracy / 2;
+  L.marker(e.latlng).addTo(map)
+	.bindPopup("You are within " + radius + " meters from this point").openPopup();
+  L.circle(e.latlng, radius).addTo(map);
 
-				console.log(result);
+}
 
-				if (result.status.name == "ok") {
+map.on('locationfound', onLocationFound);
+map.locate({setView: true, watch: true, maxZoom: 13});
 
-					$('#txtFeature').html(result['data'][0]['feature']);
-					$('#txtLng').html(result['data'][0]['lng']);
-					$('#txtLat').html(result['data'][0]['lat']);
-					$('#txtCountryCode').html(result['data'][0]['countryCode']);
-					$('#txtThumbnailImg').html(result['data'][0]['thumbnailImg']);
 
-				}
-			
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				// your error code
-			}
-		}); 
-	
 
-	});
 
-	$('#btnRun3').click(function() {
 
-		$.ajax({
-			url: "php/getNearbyInfo.php",
-			type: 'POST',
-			dataType: 'json',
-			data: {
-				lng: $('#selNearbyLng').val(),
-				lat: $('#selNearbyLat').val()
-			},
-			success: function(result) {
+//ajax call
+$(document).ready(function(){
 
-				console.log(result);
+ 
 
-				if (result.status.name == "ok") {
+  $.ajax({
 
-					$('#txtTitle').html(result['data'][0]['title']);
-					$('#txtSummary').html(result['data'][0]['summary']);
-					$('#txtDistance').html(result['data'][0]['distance']);
-					$('#txtWikipediaUrl').html(result['data'][0]['wikipediaUrl']);
+      type: 'POST',
 
-				}
-			
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				// your error code
-			}
-		}); 
-	
+      url: 'php/ctr.php',
 
-	});
+      dataType: 'json',
+
+      success: function(result) {
+
+
+
+          $('#countrySelect').html('');
+
+
+
+          $.each(result.data, function(index) {
+
+          
+
+              $('#countrySelect').append($("<option>", {
+
+                  value: result.data[index].code,
+
+                  text: result.data[index].name
+
+              })); 
+
+          
+
+          }); 
+
+      }
+
+  })
+});
+
+
+// border
+
+const border = L.border('border');
+
+if (map.hasLayer(border)) {
+
+  map.removeLayer(border);
+
+}
+
+
+
+border = L.geoJson(result.data,{
+
+  color: '#ff7800',
+
+  weight: 2,
+
+  opacity: 0.65
+
+}).addTo(map);         
+
+
+
+map.fitBounds(border.getBounds());
+
+
+//listener to select
+
+document.getElementById('countrySelect').html; //listener should get countrySelect or border??
+
+
+//another ajax call to get border??
+$(document).ready(function(){
+
+ 
+
+  $.ajax({
+
+      type: 'POST',
+
+      url: 'php/getBorder.php',     //to get border ??
+
+      dataType: 'json',
+
+      success: function(result) {
+
+
+
+          $('#countrySelect').html('');
+
+
+
+          $.each(result.data, function(index) {
+
+          
+
+              $('#countrySelect').append($("<option>", {
+
+                  value: result.data[index].code,
+
+                  text: result.data[index].name
+
+              })); 
+
+          
+
+          }); 
+
+      }
+
+  })
+});
