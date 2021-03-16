1f4c7ff9d5ed7cacdefd05233cdf5871503ac211
ar2-url <57313716+ar2-url@users.noreply.github.com>
2021-03-16 10:13:27 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
-// map
+$('document').ready(function(){
 
-// Initialize the map 
+	// map
+
+	// Initialize the map 
 const map = L.map('map');
   
-// Get the tile layer from OpenStreetMaps 
+	// Get the tile layer from OpenStreetMaps 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { 
 
-// Specify the maximum zoom of the map 
+	// Specify the maximum zoom of the map 
 maxZoom: 19, 
 
-// Set the attribution for OpenStreetMaps 
+	// Set the attribution for OpenStreetMaps 
 attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' 
 }).addTo(map); 
 
-// Set the view of the map 
-// with the latitude, longitude and the zoom value 
-//map.setView([48.8584, 2.2945], 16); 
+	// Set the view of the map 
+	// with the latitude, longitude and the zoom value e.g
+	//map.setView([48.8584, 2.2945], 16); 
   
 
 
-// Set the map view to the user's location 
-// Uncomment below to set map according to user location 
+	// Set the map view to the user's location 
 map.locate({setView: true, maxZoom: 13}); 
 
 
@@ -37,71 +38,9 @@ map.on('locationfound', onLocationFound);
 map.locate({setView: true, watch: true, maxZoom: 13});
 
 
-// document.ready
-$(document).ready(function(){
-
-
-  $.ajax({
-
-      type: 'POST',
-
-      url: 'php/ctr.php',
-
-      dataType: 'json',
-
-      cache: false,
-
-      async: true,
-
-      success: function(result) {
-        console.log(result);
-
-				if (result.status.name == "ok") {
-
-					
-
-          $('#countrySelect').html(result['data'][0]['name']);
-        
-          $.ajax({
-            type: 'POST',
-            url: 'php/getBorder.php',
-            dataType: 'json',
-            cache: false,
-            async: true,
-
-            success: function(result) {
-
-        
-              console.log(result);
-      
-              if (result.status.name == "ok") {
-      
-      
-                $('#countrySelect').html(result['data'][0]['code']);
-
-        }
-
-
-
-          $.each(result.data, function(index) {
-
-          
-
-              $('#countrySelect').append($("<option>", {
-
-                  value: result.data[index].code,
-
-                  text: result.data[index].name
-
-              })); 
-
-          
-
-          }); 
-
-      }
-
-  })
-});
-
+if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      var latLong = latitude + ',' + longitude;
 
